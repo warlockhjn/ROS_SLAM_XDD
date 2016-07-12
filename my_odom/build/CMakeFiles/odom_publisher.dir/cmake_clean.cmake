72f686db59dcd7709e368e4bf4c8044c6b94705e
warlockhjn <jenningshu@outlook.com>
2016-07-12 10:30:36 +0800

    odom_pubisher and urdf
    添加了里程计输入接口节点odom_publisher，在rviz中粗略的画出了机器人，并仿真其圆周运动。

---
+++ my_odom/build/CMakeFiles/odom_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/odom_publisher.dir/src/odom_publisher.cpp.o"
+  "../bin/odom_publisher.pdb"
+  "../bin/odom_publisher"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/odom_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
