b891e7a0fd2437a6febf225ea15c835d99be1a90
warlockhjn <jenningshu@outlook.com>
2016-07-12 16:46:02 +0800

    Revert "modify odom_publisher.cpp"
    This reverts commit 80813f23bfdfc04f0842605f8c9c78142e884afd.

--- my_odom/src/odom_publisher.cpp
+++ my_odom/src/odom_publisher.cpp
@@ -1,82 +1,81 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
-#include <std_msgs/Float64MultiArray.h>
 
-double vx = 0.0, vy = 0.0, vth = 0.0;
+int main(int argc, char** argv){
+  ros::init(argc, argv, "odometry_publisher");
 
-void receive_speed(const std_msgs::Float64MultiArray::ConstPtr& msg)
-{
-    vx = msg->data[0];
-    vy = msg->data[1];
-    vth = msg->data[2];
-}
+  ros::NodeHandle n;
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  tf::TransformBroadcaster odom_broadcaster;							// send message out using ROS and tf respectively
 
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "odometry_publisher");
+  double x = 0.0;
+  double y = 0.0;
+  double th = 0.0;														// robot start at the origin of the "odom" coordinate frame initially
 
-    ros::NodeHandle n;
-    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-    ros::Subscriber odom_sub = n.subscribe("from_odom", 50, receive_speed);
-    tf::TransformBroadcaster odom_broadcaster;
+  double vx = 0.1;														// fake robot move in the odome frame at a rate of 0.1m/s in x, -0.1m/s in y, 
+  double vy = -0.1;														// 0.1rad/s in the h direction, which will cause the robot drive in a circle. 
+  double vth = 0.1;
+  
+  ros::Time current_time, last_time;
+  current_time = ros::Time::now();
+  last_time = ros::Time::now();
 
-    double x = 0.0;
-    double y = 0.0;
-    double th = 0.0;
+  ros::Rate r(5);														// publish the odometry information at a rate of 5Hz
+  while(n.ok()){
 
-    ros::Time current_time, last_time;
+    ros::spinOnce();               // check for incoming messages
     current_time = ros::Time::now();
-    last_time = ros::Time::now();
 
-    ros::Rate r(5);
-    while(n.ok())
-    {
-        vx = vy = vth = 0.0;
+    // compute odometry in a typical way given the velocities of the robot
+    double dt = (current_time - last_time).toSec();
+    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+    double delta_th = vth * dt;
+	// A real odometry system would, of course, integrate computed velocities instead.
+	
+    x += delta_x;
+    y += delta_y;
+    th += delta_th;
 
-        ros::spinOnce();
-        current_time = ros::Time::now();
+    // since all odometry is 6DOF we'll need a quaternion created from yaw
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
-        double dt = (current_time - last_time).toSec();
-        double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-        double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-        double delta_th = vth * dt;
+    //first, we'll publish the transform over tf
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.stamp = current_time;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
 
-        x += delta_x;
-        y += delta_y;
-        th += delta_th;
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
+    odom_trans.transform.translation.z = 0.0;
+    odom_trans.transform.rotation = odom_quat;
 
-        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+    //send the transform
+    odom_broadcaster.sendTransform(odom_trans);
 
-        geometry_msgs::TransformStamped odom_trans;
-        odom_trans.header.stamp = current_time;
-        odom_trans.header.frame_id = "odom";
-        odom_trans.child_frame_id = "base_link";
+    //next, we'll publish the odometry message over ROS
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
 
-        odom_trans.transform.translation.x = x;
-        odom_trans.transform.translation.y = y;
-        odom_trans.transform.translation.z = 0.0;
-        odom_trans.transform.rotation = odom_quat;
+    //set the position
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.orientation = odom_quat;
 
-        odom_broadcaster.sendTransform(odom_trans);
+    //set the velocity
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x = vx;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vth;
 
-        nav_msgs::Odometry odom;
-        odom.header.stamp = current_time;
-        odom.header.frame_id = "odom";
+    //publish the message
+    odom_pub.publish(odom);
 
-        odom.pose.pose.position.x = x;
-        odom.pose.pose.position.y = y;
-        odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation = odom_quat;
-
-        odom.child_frame_id = "base_link";
-        odom.twist.twist.linear.x = vx;
-        odom.twist.twist.linear.y = vy;
-        odom.twist.twist.angular.z = vth;
-
-        odom_pub.publish(odom);
-
-        last_time = current_time;
-        r.sleep();
-    }
+    last_time = current_time;
+    r.sleep();
+  }
 }
